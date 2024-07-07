cd9b755ae029ab924a134cf8bd8d1df984455036
Faouzia-28 <faouziaindira@gmail.com>
2024-07-07 15:13:20 +0530

    This is trial

---
+++ fib.py
@@ -0,0 +1,16 @@
+def main():
+    n=int(input())
+    a=0
+    b=1
+    print("Fibonacci sequence:")
+    print(a,end=" ")
+    print(b,end=" ")
+    for i in range(n-2):
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
+        
+
+if __name__ == "__main__":
+    main()
